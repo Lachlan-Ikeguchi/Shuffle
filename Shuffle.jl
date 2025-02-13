28389e768f955872ca5de2638c473c335e44cfe0
Lachlan <lachlan_ikeguchi@proton.me>
2025-02-13 14:55:36 +1000

    Working on multithreading

--- Shuffle.jl
+++ Shuffle.jl
@@ -17,9 +17,10 @@ end
 
 function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UInt8)::Float64
     optimal_distance = number_of_suits
-    distances = []
+    distances = Vector{Float64}(undef, (number_of_suits * (size_of_suits - 1) + size_of_suits * (size_of_suits - 1)))
 
-    Threads.@threads for suit in number_of_suits
+    Threads.@threads for suit in 1:number_of_suits
+        println("Working on suit: ", suit, " thead ID: ", Threads.threadid())
         last_card_index = 0
         for card in 1:size_of_suits
             step_from_last_card = 1
@@ -32,7 +33,7 @@ function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UI
                     end
                 else
                     if deck[last_card_index + step_from_last_card].suit == suit
-                        push!(distances, step_from_last_card)
+                        distances[suit + card] = step_from_last_card
                         last_card_index += step_from_last_card
                         found = true
                     end
@@ -41,9 +42,11 @@ function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UI
                 step_from_last_card += 1
             end
         end
+        println("Finished working on suit: ", suit, " thread ID: ", Threads.threadid())
     end
 
-    Threads.@threads for number in size_of_suits
+    Threads.@threads for number in 1:size_of_suits
+        println("Working on numbers: ", number, " thead ID: ", Threads.threadid())
         last_card_index = 0
         for card in 1:size_of_suits
             step_from_last_card = 1
@@ -56,13 +59,16 @@ function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UI
                     end
                 else
                     if deck[last_card_index + step_from_last_card].number == number
-                        push!(distances, step_from_last_card)
+                        distances[number_of_suits * (size_of_suits - 1) + number + card] = step_from_last_card
                         last_card_index += step_from_last_card
                         found = true
                     end
                 end
+
+                step_from_last_card += 1
             end
         end
+        println("Finished working on numbers: ", number, " thread ID: ", Threads.threadid())
     end
     
     average_distance = sum(distances) / length(distances)
@@ -72,6 +78,8 @@ end
 
 const number_of_suits::UInt8 = 4
 const size_of_suits::UInt8 = 13
+
+println("Working with: ", Threads.nthreads(), " theads")
 
 deck = generate_deck(number_of_suits, size_of_suits)
 println(uniformity_index(deck, number_of_suits, size_of_suits))
