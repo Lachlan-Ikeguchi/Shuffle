5148363bf8d9455c0804677b4ebaf6abc2a56bf3
Lachlan Ikeguchi <lachlan_ikeguchi@proton.me>
2025-02-14 15:33:46 +1000

    debugging arrays

--- Shuffle.jl
+++ Shuffle.jl
@@ -1,3 +1,5 @@
+using Random
+
 struct card
     suit
     number
@@ -15,14 +17,27 @@ function generate_deck(number_of_suits::UInt8, size_of_suits::UInt8)::Array
     return deck
 end
 
+function perfect_deck(number_of_suits::UInt8, size_of_suits::UInt8)::Array
+    deck = []
+
+    for number in 1:size_of_suits
+        for suit in 1:number_of_suits
+            push!(deck, card(suit, number))
+        end
+    end
+
+    return deck
+end
+
 function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UInt8)::Float64
-    optimal_distance = number_of_suits
-    distances = Vector{Float64}(undef, (number_of_suits * (size_of_suits - 1) + size_of_suits * (size_of_suits - 1)))
+    optimal_distance_suits = number_of_suits
+    optimal_distance_numbers = size_of_suits
+    distances_suits = Vector{Int}(undef, (number_of_suits * (size_of_suits - 1)))
+    distances_numbers = Vector{Int}(undef, (size_of_suits * (number_of_suits - 1)))
 
     Threads.@threads for suit in 1:number_of_suits
-        println("Working on suit: ", suit, " thead ID: ", Threads.threadid())
         last_card_index = 0
-        for card in 1:size_of_suits
+        for number in 1:size_of_suits
             step_from_last_card = 1
             found = false
             while !found
@@ -32,23 +47,22 @@ function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UI
                         found = true
                     end
                 else
-                    if deck[last_card_index + step_from_last_card].suit == suit
-                        distances[suit + card] = step_from_last_card
+                    if deck[last_card_index+step_from_last_card].suit == suit
+                        distances_suits[suit+number] = round(step_from_last_card) 
                         last_card_index += step_from_last_card
                         found = true
+                        println(distances_suits[suit+number])
                     end
                 end
 
                 step_from_last_card += 1
             end
         end
-        println("Finished working on suit: ", suit, " thread ID: ", Threads.threadid())
     end
 
     Threads.@threads for number in 1:size_of_suits
-        println("Working on numbers: ", number, " thead ID: ", Threads.threadid())
         last_card_index = 0
-        for card in 1:size_of_suits
+        for suit in 1:number_of_suits
             step_from_last_card = 1
             found = false
             while !found
@@ -58,28 +72,38 @@ function uniformity_index(deck::Array, number_of_suits::UInt8, size_of_suits::UI
                         found = true
                     end
                 else
-                    if deck[last_card_index + step_from_last_card].number == number
-                        distances[number_of_suits * (size_of_suits - 1) + number + card] = step_from_last_card
+                    if deck[last_card_index+step_from_last_card].number == number
+                        distances_numbers[number+suit] = round(step_from_last_card)
                         last_card_index += step_from_last_card
                         found = true
+                        println(distances_numbers[suit+number])
                     end
                 end
 
                 step_from_last_card += 1
             end
         end
-        println("Finished working on numbers: ", number, " thread ID: ", Threads.threadid())
     end
     
-    average_distance = sum(distances) / length(distances)
+    println(distances_suits, "\n")
+    println(distances_numbers, "\n")
 
-    return average_distance / optimal_distance
+    suit_index = (sum(distances_suits) / length(distances_suits)) / optimal_distance_suits
+    number_index = (sum(distances_numbers) / length(distances_numbers)) / optimal_distance_numbers
+
+    return suit_index * number_index
 end
 
 const number_of_suits::UInt8 = 4
 const size_of_suits::UInt8 = 13
 
-println("Working with: ", Threads.nthreads(), " theads")
-
 deck = generate_deck(number_of_suits, size_of_suits)
-println(uniformity_index(deck, number_of_suits, size_of_suits))
+println(deck, "\n")
+index = uniformity_index(deck, number_of_suits, size_of_suits)
+println(index)
+
+# perfect = perfect_deck(number_of_suits, size_of_suits)
+# println(uniformity_index(perfect, number_of_suits, size_of_suits))
+
+# shuffled = Random.shuffle(deck)
+# println(uniformity_index(shuffled, number_of_suits, size_of_suits))
